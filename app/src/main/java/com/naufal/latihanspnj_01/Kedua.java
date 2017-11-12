a8fc25fc6f5f27f719d59719b2cb5dde0ca73314
naufalmms <naufalmarzuk@gmail.com>
2017-11-13 02:36:03 +0700

    kedua

--- app/src/main/java/com/naufal/latihanspnj_01/Kedua.java
+++ app/src/main/java/com/naufal/latihanspnj_01/Kedua.java
@@ -1,7 +1,10 @@
 package com.naufal.latihanspnj_01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Kedua extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Kedua extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kedua);
+
+        Button next = findViewById(R.id.bttn2);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Awal.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 }
